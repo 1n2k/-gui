ccce8a5e87fe0e116229a5dc20f9faf596d81928
sevenghosts <ernst-007@gmx.de>
2014-12-15 08:25:24 +0100

    tgxck2

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,36 @@
 #include "main.hpp"
+#include <cmath>
 #include <cstdio>
 
 std::vector<point> coordinates;
 std::vector<std::vector<unsigned long long>> distances;
+std::vector<int> callCounts;
 
-
-point crystal_meth_ods(point s, double xr, int d, int e, int f)
+#define sq( a ) ( ( a ) * ( a ) )
+#define x( idx ) ( callCounts[ idx ] ? ( coordinates[ idx ].x / callCounts[ idx ] ) : coordinates[ idx ].x )
+#define y( idx ) ( callCounts[ idx ] ? ( coordinates[ idx ].y / callCounts[ idx ] ) : coordinates[ idx ].y )
+void calculatePositions( int p_p1, int p_p2 )
 {
-    double yr = math.sqrt(math.pow(d,2)-math.pow(xr-s.x,2)+s.y);
-    double xm = ((math.pow(f,2)-math.pow(e,2))+(math.pow(s.x,2)-math.pow(xr,2))/(2*(s.x-xr));
-    double ym = ((math.pow(f,2)-math.pow(e,2))+(math.pow(s.y,2)-math.pow(yr,2))/(2*(s.y-yr));
-    return {xm, ym};
+    double xs = x( p_p1 );
+    double ys = y( p_p1 );
+    double xr = x( p_p2 );
+
+    unsigned long long d = distances[ p_p1 ][ p_p2 ];
+
+    double yr = ( ( coordinates[ p_p2 ].y + sqrt( sq( d ) - sq( xr - xs ) + ys ) ) / ++callCounts[ p_p2 ] ) ;
+    coordinates[ p_p2 ].x += xr;
+
+    for( int m = 0; m < (int)coordinates.size( ); ++m ) {
+        if( m == p_p1 || m == p_p2 ) continue;
+
+        unsigned long long e = distances[ p_p1 ][ m ];
+        unsigned long long f = distances[ p_p2 ][ m ];
+
+        coordinates[ m ].x += ( sq( f ) - sq( e ) + sq( xs ) - sq( xr ) ) / ( 2 * ( xs - xr ) );
+        coordinates[ m ].y += ( sq( f ) - sq( e ) + sq( ys ) - sq( yr ) ) / ( 2 * ( ys - yr ) );
+
+        callCounts[ m ]++;
+    }
 };
 
 void getPointCount( ) {
